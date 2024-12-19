6032982773d54e3e6b5cda79700995e1e8e60506
Marcin <marcin.konow@lczyk.xyz>
2024-12-19 12:12:18 +0000

    part2 done

--- src/day01/part2.zig
+++ src/day01/part2.zig
@@ -11,23 +11,42 @@ pub fn main() !void {
     const in = try stdin.readAllStdin(alloc);
     defer alloc.free(in);
 
-    const lines = try stdin.splitLines(alloc, in);
-    defer alloc.free(lines);
-
-    for (lines, 0..) |line, i| {
-        print("{d}: {s}\n", .{ i, line });
+    const id_type = u32;
+    var it = std.mem.tokenizeAny(u8, in, " \n");
+    var ids = std.ArrayList(id_type).init(alloc);
+    while (it.next()) |token| {
+        const value = try std.fmt.parseInt(id_type, token, 10);
+        try ids.append(value);
     }
 
-    const answer = get_answer();
+    // Frequency for all the numbers in the right list
+    var frequency = std.AutoHashMap(id_type, u32).init(alloc);
+    defer frequency.deinit();
+    var i: usize = 1;
+    while (i < ids.items.len) : (i += 2) {
+        const id = ids.items[i];
+        if (frequency.get(id)) |count| {
+            try frequency.put(id, count + 1);
+        } else {
+            try frequency.put(id, 1);
+        }
+    }
 
+    var sum: @Type(.{ .Int = .{
+        .bits = @typeInfo(id_type).Int.bits * 2,
+        .signedness = .unsigned,
+    } }) = 0;
+
+    i = 0;
+    while (i < ids.items.len) : (i += 2) {
+        const id = ids.items[i];
+        const count = frequency.get(id) orelse 0;
+        const score = count * id;
+        // print("id: {}, count: {}, score: {}\n", .{ id, count, score });
+        sum += score;
+    }
+
+    const answer = sum;
     const stdout = std.io.getStdOut().writer();
     try stdout.print("{d}", .{answer});
-}
-
-fn get_answer() u8 {
-    return 255;
-}
-
-test "test getting answer" {
-    try testing.expect(get_answer() == 255);
 }
