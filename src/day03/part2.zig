fce2b0c78e9591fdfb822a794298d65d929caca0
Marcin <marcin.konow@lczyk.xyz>
2024-12-26 22:07:14 +0000

    more refactor

--- src/day03/part2.zig
+++ src/day03/part2.zig
@@ -6,6 +6,34 @@ const stdin = @import("stdin");
 
 const utils = @import("utils.zig");
 
+fn Pattern(comptime N: u8) type {
+    return struct {
+        const Self = @This();
+
+        want: [N]utils.TokenKind,
+        stack: [N]utils.Token = undefined,
+        i: usize = 0,
+
+        pub fn reset(self: *Self) void {
+            self.i = 0;
+            self.stack = undefined;
+        }
+
+        pub fn push(self: *Self, token: utils.Token) void {
+            self.stack[self.i] = token;
+            self.i += 1;
+        }
+
+        pub fn current(self: *Self) utils.TokenKind {
+            return self.want[self.i];
+        }
+
+        pub fn atEnd(self: *Self) bool {
+            return self.i == N;
+        }
+    };
+}
+
 pub fn main() !void {
     const alloc = std.heap.page_allocator;
 
@@ -19,39 +47,36 @@ pub fn main() !void {
 
     var sum: u32 = 0;
 
-    var stack: [6]utils.Token = undefined;
-    const want: [6]utils.TokenKind = .{
+    var mul: Pattern(6) = .{ .want = .{
         utils.TokenKind.Mul,
         utils.TokenKind.Lbrace,
         utils.TokenKind.Number,
         utils.TokenKind.Comma,
         utils.TokenKind.Number,
         utils.TokenKind.Rbrace,
-    };
-    var i: usize = 0;
+    } };
+
     for (scanner.tokens.items) |token| {
-        if (token.tag == want[i]) {
+        if (token.tag == mul.current()) {
+            mul.push(token);
             // push the token to the stack
-            stack[i] = token;
-            i += 1;
         } else {
             // clear the stack
-            i = 0;
-            stack[i] = token;
+            mul.reset();
         }
 
-        if (i == 6) {
+        if (mul.atEnd()) {
             // Evaluate the expression
-            print("Evaluating expression: {s}\n", .{in[stack[0].loc.start..stack[stack.len - 1].loc.end]});
-            const a = try std.fmt.parseInt(u32, in[stack[2].loc.start..stack[2].loc.end], 10);
-            const b = try std.fmt.parseInt(u32, in[stack[4].loc.start..stack[4].loc.end], 10);
+            print("Evaluating expression: {s}\n", .{in[mul.stack[0].loc.start..mul.stack[mul.stack.len - 1].loc.end]});
+            const a = try std.fmt.parseInt(u32, in[mul.stack[2].loc.start..mul.stack[2].loc.end], 10);
+            const b = try std.fmt.parseInt(u32, in[mul.stack[4].loc.start..mul.stack[4].loc.end], 10);
 
             const result = a * b;
             // print("{d} = {d} * {d}\n", .{ result, a, b });
             sum += result;
 
             // Reset the stack
-            i = 0;
+            mul.reset();
         }
     }
 
